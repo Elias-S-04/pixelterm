325da2759464a95763d74d9d00af07dafb951100
Elias Sørås <eliaslvsrs@gmail.com>
2025-10-25 15:07:09 +0200

    Changed filled form true to false

--- examples/demo_circle.py
+++ examples/demo_circle.py
@@ -3,7 +3,7 @@ from pixelterm import PixelRenderer, draw_circle
 r = PixelRenderer(60, 30)
 try:
     for rad in range(5, 15, 3):
-        draw_circle(r, r.width // 2, r.height // 2, rad, (150, 200 - rad*5, 255), filled=True)
+        draw_circle(r, r.width // 2, r.height // 2, rad, (150, 200 - rad*5, 255), filled=False)
     r.render()
 finally:
     r.cleanup()
